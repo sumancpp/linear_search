2b2f5c5a2586920d6b9e68c3bd528f2b76f00f90
sumancpp <someone.cpp@proton.me>
2024-11-29 23:53:29 +0530

    Add files via upload

---
+++ linear_search.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int search(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+    if(arr[i]==target){
+        return i;
+    }
+   }
+   return -1;
+   }
+      
+int main(){
+     cout<<"Enter the size of the array ";
+     int size;
+     cin>>size;
+     int arr[size];
+     cout<<"Enter the elements ";
+     for(int i=0;i<size;i++){
+        cin>>arr[i];
+     }
+     int target;
+     cout<<"Enter the target ";
+     cin>>target;
+     int result=search(arr, size, target);
+     if (result !=-1){
+        cout<<"Found your target at index "<<result<<" number";
+     }
+     else{
+        cout<<"We cant find your target in the array";
+     }
+
+    
+    return 0;
+}
