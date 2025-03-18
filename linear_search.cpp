c3466d466c9ecca136804bf578522253c801cbf5
Suman Maity <someone.cpp@proton.me>
2025-03-18 23:35:26 +0530

    linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-int search(int arr[],int size,int target){
-    for(int i=0;i<size;i++){
-    if(arr[i]==target){
+int search(int arr[], int size, int target){
+    for(int i = 0; i < size; i++){
+    if(arr[i] == target){
         return i;
     }
    }
@@ -12,23 +12,21 @@ int search(int arr[],int size,int target){
 int main(){
      cout<<"Enter the size of the array ";
      int size;
-     cin>>size;
+     cin >> size;
      int arr[size];
      cout<<"Enter the elements ";
-     for(int i=0;i<size;i++){
-        cin>>arr[i];
+     for(int i = 0; i < size; i++){
+        cin >> arr[i];
      }
      int target;
      cout<<"Enter the target ";
-     cin>>target;
-     int result=search(arr, size, target);
-     if (result !=-1){
+     cin >> target;
+     int result = search(arr, size, target);
+     if (result != -1){
         cout<<"Found your target at index "<<result<<" number";
      }
      else{
         cout<<"We cant find your target in the array";
      }
-
-    
     return 0;
 }
